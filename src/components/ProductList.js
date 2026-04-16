c4f10a7e98c907f37683d00c85b30e09b89d9d79
Lakshmi Dubey <lakshmi.mcahbti@gmail.com>
2026-04-15 22:42:32 -0500

    Commit all pending shopmate changes

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,32 +1,20 @@
 import React from 'react'
 import { useState } from 'react';
-import { useEffect } from 'react';
-import { useCallback } from 'react';
+import { useFetch } from './hooks/useFetch';
 
 export const ProductList = () => {
-    const [products,setProducts]=useState([]);  
-    const [url,setUrl]=useState("http://localhost:8000/products/");   
-      const fetchProducts=useCallback(
-        async()=>{
-            const response=await fetch(url);
-            const data=await  response.json();
-            setProducts(data);
-         },[url]);
-       useEffect(()=>{
-       
-            fetchProducts();
-       },[fetchProducts]);
 
-  return (
-   <section>
-    
-       <div className='filter'>
-         <button onClick={()=>setUrl("http://localhost:8000/products/")}>All</button><span>                     </span>
-        <button onClick={()=>setUrl("http://localhost:8000/products/?in_stock=true")}>In Stock</button>
-    
-       </div>
-    {
-        products.map((product)=>(
+    const [url,setUrl]=useState("http://localhost:8000/products/");   
+    const { data: products } = useFetch(url);
+
+   return (
+    <section>
+        <div className="filter">
+            <button onClick={() => setUrl("http://localhost:8000/products")}>All</button>
+            <button onClick={() => setUrl("http://localhost:8000/products?in_stock=true")}>In Stock Only</button>
+        </div>
+        
+        { products && products.map((product) => (
             <div className="card" key={product.id}>
                 <p className="id">{product.id}</p>
                 <p className="name">{product.name}</p>
@@ -34,11 +22,8 @@ export const ProductList = () => {
                     <span>${product.price}</span>
                     <span className={product.in_stock ? "instock" : "unavailable"}>{product.in_stock ? "In Stock" : "Unavailable"}</span>
                 </p>
-            </div>      
-        )
-
-        )
-    }
-   </section>
+            </div>            
+        )) }
+    </section>
   )
 }
